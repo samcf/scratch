aa34f7e863e52fc30c7c6da2aa3923d6aed35c82
Sam Ferrell <mail@samcf.me>
2021-02-19 07:30:29 -0700

    New aliases for env vars, restart server on save.

--- src/scratch/core.clj
+++ src/scratch/core.clj
@@ -2,41 +2,38 @@
   (:require [io.pedestal.http :as http]
             [hiccup.core :as hiccup]))
 
-(defn index [_]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (hiccup/html
-          [:html
-           [:head
-            [:title "Scratch"]]
-           [:body
-            [:div {:id "scratch"}]]])})
-
-(def service
-  {:env :prod
-   ::http/join? false
-   ::http/routes #{["/" :get index :route-name :index]}
-   ::http/resource-path "/public"
-   ::http/type :jetty
-   ::http/host "0.0.0.0"
-   ::http/port (let [port (or (System/getenv "PORT") "8080")]
-                 (Integer/parseInt port 10))})
-
 (defonce server (atom nil))
+(defonce config (atom {:env :dev}))
+
+(defn index [_]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (hiccup/html [:html
+                          [:head [:title "Scratch"]]
+                          [:body [:div {:id "scratch"}]]])})
+
+(defn service-routes []
+  #{["/" :get index :route-name :index]})
+
+(defn create-service []
+  (let [port (System/getenv "PORT") env (:env @config)]
+    {:env (if (= env :dev) :dev :prod)
+     ::http/resource-path "/public"
+     ::http/routes (service-routes)
+     ::http/join? (not= env :dev)
+     ::http/type :jetty
+     ::http/host (if (= env :dev) "localhost" "0.0.0.0")
+     ::http/port (if (string? port) (Integer/parseInt port 10) 8080)}))
 
 (defn server-start []
-  (let [srvc (assoc service ::http/join? false :env :dev)
-        srvr (http/create-server srvc)
-        strt (http/start srvr)]
-    (reset! server strt)))
+  (when-not (nil? @server)
+    (http/stop @server))
 
-(defn server-stop []
-  (when (some? @server)
-    (http/stop @server)))
+  (let [service (-> (create-service) (http/create-server) (http/start))]
+    (reset! server service)))
 
-(defn server-restart []
-  (server-stop)
+(defn start [options]
+  (reset! config options)
   (server-start))
 
-(defn -main []
-  (server-start))
+(server-start)
