3544ea829f994ab4324f64cef43a62907e97f11d
Sam Ferrell <mail@samcf.me>
2021-02-25 18:49:27 -0700

    Ignore compiled script.

--- resources/public/script/scratch.js
+++
@@ -1,9 +0,0 @@
-var CLOSURE_UNCOMPILED_DEFINES = {"cljs.core._STAR_target_STAR_":"browser"};
-var CLOSURE_NO_DEPS = true;
-if(typeof goog == "undefined") document.write('<script src="script/cljs/goog/base.js"></script>');
-document.write('<script src="script/cljs/goog/deps.js"></script>');
-document.write('<script src="script/cljs/cljs_deps.js"></script>');
-document.write('<script>if (typeof goog == "undefined") console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?");</script>');
-document.write('<script>goog.require("process.env");</script>');
-document.write('<script>goog.require("clojure.browser.repl.preload");</script>');
-document.write('<script>goog.require("scratch.render");</script>');
